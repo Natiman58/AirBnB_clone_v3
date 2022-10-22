ea7cd4a37d169e0c441c5b573e2f7ecfcfeecd5f
Natiman58 <natiman58@gmail.com>
2022-10-22 13:52:33 +0300

    coustom error

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """
     A module to start a flask app
 """
-from flask import Flask
+from flask import Flask, request, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 import os
@@ -20,6 +20,16 @@ def tear_down(exception):
         clsoses the connection
     """
     storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(exception):
+    """
+        Handles the 404 not found error
+    """
+    error_code = exception.__str__().split()[0]
+    text_msg = {"error": "Not found"}
+    return make_response(jsonify(text_msg), error_code)
 
 
 if __name__ == "__main__":
