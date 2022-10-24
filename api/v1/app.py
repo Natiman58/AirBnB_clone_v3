0030817cc69068c5dc497eb8c4631d5c62a6e2ba
Natiman58 <natiman58@gmail.com>
2022-10-24 14:26:32 +0300

    allow controll enabled

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,9 +6,11 @@ from flask import Flask, request, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 host = os.getenv('HBNB_API_HOST', '0.0.0.0')
 port = os.getenv('HBNB_API_PORT', 5000)
